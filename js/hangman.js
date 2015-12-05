8859438b4e0ef75240a538711f5fcb47dfb1c9f3
Brandon O'Very <brovery@yahoo.com>
2015-12-05 14:33:26 -0700

    Progress

--- js/hangman.js
+++ js/hangman.js
@@ -3,16 +3,31 @@
  */
 var words = ["library","expensive","divided","transportation","famous","camera"],
     wordCount = 0,
+    word = words[wordCount],
+    voices,
+    wrong = 0,
+    guesses = 6,
+    right = 0,
+    guessed = [],
+    wrongLetter = new Speech("Nope!"),
+    correct = new Speech("Correct!"),
     currentWord = new Speech(words[wordCount]),
+    win = new Speech("You Win! Huzzah!"),
+    loser = new Speech("You lose! That poor guy is dead, all because of you!"),
     spellCount = 0;
 
-//jQuery(document).ready(function(){
-//    nextWord();
-//});
-
 function nextWord() {
+    wordCount++;
+    spellCount = 0;
     currentWord = new Speech(words[wordCount]);
-    currentWord.build();
+    word = words[wordCount];
+    $('#nextWord').hide();
+    $('#alphabet').hide();
+    $('#hangman').hide();
+    $('#hangWord').hide();
+    $('.spellCont').show();
+
+
 }
 
 function playMe() {
@@ -22,13 +37,11 @@ function playMe() {
 function Speech(message) {
     var msg = new SpeechSynthesisUtterance(message);
     msg.lang = 'en';
-
     window.speechSynthesis.onvoiceschanged = function() {
-        var voices = window.speechSynthesis.getVoices();
-        msg.voice = voices[1];
+        voices = window.speechSynthesis.getVoices();
     };
-
     this.say = function() {
+        msg.voice = voices[1];
         window.speechSynthesis.speak(msg);
     }
 }
@@ -45,6 +58,7 @@ function spellCheck() {
     }
 
     if (spellCount == 3) {
+        spaceBuilder();
         $('.spellCont').hide();
         $('#hangman').show();
         $('#alphabet').show();
@@ -59,7 +73,7 @@ function spellCheck() {
 //function to build the word-spaces.
 function spaceBuilder() {
     var myDiv = "";
-    var len = (word.length*70); //Set the width necessary for the word length.
+    var len = word.length*70; //Set the width necessary for the word length.
     var lMarg = (1140-len)/2; //Set the left margin such that the word is centered.
     len += "px";
 
@@ -67,12 +81,59 @@ function spaceBuilder() {
     for (var i = 0; i<word.length; i++) {
         myDiv += "<div class='" + word[i] + " space'></div>";
     }
-    document.getElementById("hangWord").innerHTML = myDiv;
-    $("#hangWord").css("width", len).css("margin-left", lMarg);
+    $("#hangWord").html(myDiv).css("width", len).css("margin-left", lMarg);
 }
 
+//Function that accepts a letter from a button on the page.
+function sendLetter(theletter) {
+    document.getElementById(theletter).style.backgroundColor = "#473AFF";
+    //This if statement takes care of guessing the same letter multiple times.
+    if (guessed.indexOf(theletter) == -1) {
+        guessed.push(theletter); //Pushes the guessed letter into an array.
+    } else {
+        alert("You already guessed that letter! Try again!");
+        return;
+    }
 
+    //Check if the letter is in the word. Change the letter to lowercase.
+    if (word.search(theletter.toLowerCase()) == -1) {
+        //if the letter isn't a match, this is what happens.
+        wrongLetter.say();
+        noMatch();
+    } else {
+        correct.say();
+        match(theletter);
+    }
+}
 
+//Function determines what happens if the chosen letter is not in the word.
+function noMatch() {
+    //This changes the image on a wrong guess and progresses the guesses.
+    wrong++;
+    document.getElementById("hangmanImg").innerHTML = "<img src='images/Hangman-"+wrong+".png'/>";
+    //If you've run out of guesses, you lose. Game then resets.
+    if (wrong == guesses) {
+        loser.say();
+        $('#nextWord').show();
+        //location.reload();
+    }
+}
+
+//function that determines what happens if there is a match in the word.
+function match(ltr){
+    ltr = ltr.toLowerCase();
+    var matches = document.getElementsByClassName(ltr);
+    for (var i = 0; i < matches.length; i++){
+        matches[i].innerHTML = ltr.toUpperCase();
+        right++;
+    }
+    //Need logic to tell us if the word has been guessed correctly.
+    if (right == word.length) {
+        win.say();
+        $('#nextWord').show();
+        //location.reload();
+    }
+}
 
 
 // TODO: This is all the old code. Refactor and add it back in a little at a time, to make the hangman stuff work.
@@ -84,7 +145,7 @@ function spaceBuilder() {
 //    guesses = 6,
 //    model = "",
 //    right = 0,
-//    guessed = [],
+//
 //    win = new Audio('audio/cheer.wav'),
 //    correct = new Audio('audio/correct.mp3'),
 //    wrongLetter = new Audio('audio/wrong.wav'),
@@ -110,87 +171,4 @@ function spaceBuilder() {
 //    spaceBuilder();
 //}
 //
-////function to build the word-spaces.
-//function spaceBuilder() {
-//    var myDiv = "";
-//    var len = (word.length*70); //Set the width necessary for the word length.
-//    var lMarg = (1140-len)/2; //Set the left margin such that the word is centered.
-//    len += "px";
 //
-//    //Need to build the divs that go into the word space. Class will be space for styling & the lower-case letter.
-//    for (var i = 0; i<word.length; i++) {
-//        myDiv += "<div class='" + word[i] + " space'></div>";
-//    }
-//    document.getElementById("hangWord").innerHTML = myDiv;
-//    $("#hangWord").css("width", len).css("margin-left", lMarg);
-//}
-//
-////TODO: Add a function that adds an audio file that plays when the "Play" button is pressed.
-//function playMe() {
-//    getAudio();
-//}
-//
-//
-////pulls the audio file from the dictionary api
-//function getAudio() {
-//    var xhttp = new XMLHttpRequest();
-//    xhttp.onreadystatechange = function() {
-//        if (xhttp.readyState == 4 && xhttp.status == 200) {
-//            model = JSON.parse(xhttp.responseText);
-//        }
-//    };
-//    xhttp.open("GET", myApiRequest, true);
-//    xhttp.send();
-//}
-//
-////Function that accepts a letter from a button on the page.
-//function sendLetter(theletter) {
-//    document.getElementById(theletter).style.backgroundColor = "#473AFF";
-//    //This if statement takes care of guessing the same letter multiple times.
-//    if (guessed.indexOf(theletter) == -1) {
-//        guessed.push(theletter); //Pushes the guessed letter into an array.
-//    } else {
-//        alert("You already guessed that letter! Try again!");
-//        return;
-//    }
-//
-//    //Check if the letter is in the word. Change the letter to lowercase.
-//    if (word.search(theletter.toLowerCase()) == -1) {
-//        //if the letter isn't a match, this is what happens.
-//        wrongLetter.play();
-//        noMatch();
-//    } else {
-//        correct.play();
-//        match(theletter);
-//    }
-//}
-//
-////Function determines what happens if the chosen letter is not in the word.
-//function noMatch() {
-//    //This changes the image on a wrong guess and progresses the guesses.
-//    wrong++;
-//    document.getElementById("hangmanImg").innerHTML = "<img src='images/Hangman-"+wrong+".png'/>";
-//    //If you've run out of guesses, you lose. Game then resets.
-//    if (wrong == guesses) {
-//        loser.play();
-//        alert("Poor guy got hung! You lose! The word was: "+word+". Spell this three times, then click OK!");
-//        location.reload();
-//    }
-//}
-//
-////function that determines what happens if there is a match in the word.
-//function match(ltr){
-//    ltr = ltr.toLowerCase();
-//    //TODO: Need a way to change all of the matches when there are multiples.
-//    var matches = document.getElementsByClassName(ltr);
-//    for (var i = 0; i < matches.length; i++){
-//        matches[i].innerHTML = ltr.toUpperCase();
-//        right++;
-//    }
-//    //Need logic to tell us if the word has been guessed correctly.
-//    if (right == word.length) {
-//        win.play();
-//        alert("HOORAY! YOU WIN! Please spell the word out loud three times, then click OK.");
-//        location.reload();
-//    }
-//}
